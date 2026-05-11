cf39a602564524ddef0bee1dad6a55297468fdcd
AlejandroCEsquivel <cesar.alejandrocesquivel@gmail.com>
2026-05-11 08:49:26 -0600

    update fire.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDL5SEiBwx6Y10YIHZTrdFWU5B5YCHaLhA",
-  authDomain: "test-transporte-vargas.firebaseapp.com",
-  projectId: "test-transporte-vargas",
-  storageBucket: "test-transporte-vargas.firebasestorage.app",
-  messagingSenderId: "470070126417",
-  appId: "1:470070126417:web:607c1db7aa3e6b64719570",
-  measurementId: "G-361QC84JFD"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+// Exportar servicios
 export const auth = getAuth(app);
+export const db = getFirestore(app);
